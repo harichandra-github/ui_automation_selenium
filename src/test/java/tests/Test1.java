805bd5ef5744a1916e32c5c0e6875f2226d4dbb7
harichandra chaudhari <harichandra@bebotechnologies.com>
2023-12-18 00:00:19 +0530

    added testng,selenium dependency

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://tutorialsninja.com/demo/");
+
+	}
+
+}
